1c1d852f4bee0a9c338635a84ce24626857f3df7
Chad Taylor <taylor.thomas.c@gmail.com>
2019-11-03 16:18:04 -0600

    Extract implementation-version from middleware implementation

--- src/ring/middleware/version.clj
+++ src/ring/middleware/version.clj
@@ -1,25 +1,19 @@
 (ns ring.middleware.version
-  (:require [ring.util.response :as resp]))
+  (:require [ring.util.response :as response]))
 
-(gen-class
- :name ring.middleware.version.Version
- :methods [])
+(defn wrap-version-header [handler version]
+  (fn [request]
+    (-> (handler request)
+        (response/header "X-Version" version))))
 
-(defn- implementation-version []
-  (try
-    (-> ring.middleware.version.Version
-        .getPackage
-        .getImplementationVersion)
-    (catch Exception _
-      "dev")))
-
-(defn wrap-version-response
-  "A middleware that adds the ImplementationVersion from the jar manifest to an
-  X-Version header. Returns 'dev' if no appropriate version is found (such as
-  when running at the REPL)."
-  [handler]
-  (let [version (implementation-version)]
-    (fn [request]
-      (-> request
-          handler
-          (resp/header "X-Version" version)))))
+(defn implementation-version
+  ([type]
+   (implementation-version type nil))
+  ([type default]
+   (try
+     (or (-> type
+             .getPackage
+             .getImplementationVersion)
+         default)
+     (catch Exception _
+       default))))
